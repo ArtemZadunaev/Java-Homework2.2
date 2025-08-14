6433a7671cb7189eb1bb42da3266eb8abf91fdfb
Erizard <Azgarot22@gmail.com>
2025-08-14 07:11:15 +0300

    Upd

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,15 @@ import java.net.SocketOption;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-int balance = 100;
-int payment = 100;
-int bonusCost = 100;
-if(payment > 1000) {
-    balance += payment + payment/bonusCost;
-    System.out.println("Ваш баланс: " + balance + "\nВам начислено: "+ payment/bonusCost + " баллов.");
-                   }
-else{
-    balance += payment;
-    System.out.println("Ваш баланс: " + balance + "\nВам начислено: 0 баллов.");
-    }
+        int balance = 100;
+        int payment = 100;
+        int bonusCost = 100;
+        if (payment > 1000) {
+            balance += payment + payment / bonusCost;
+            System.out.println("Ваш баланс: " + balance + "\nВам начислено: " + payment / bonusCost + " баллов.");
+        } else {
+            balance += payment;
+            System.out.println("Ваш баланс: " + balance + "\nВам начислено: 0 баллов.");
+        }
     }
 }
